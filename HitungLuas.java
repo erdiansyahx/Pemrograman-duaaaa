c7914f24f2b2187f8b7f598cf3673e02dfb7cf8c
erdiansyahx <mochamaderdiansyah@gmail.com>
2019-01-25 10:40:48 +0700

    File Kedua

--- HitungLuas.java
+++ HitungLuas.java
@@ -14,7 +14,25 @@ public class HitungLuas {
 
     /**
      * @param args the command line arguments
-     */  
+     */ 
+
+   public static void luasPersegiPanjang() {
+        int panjang;
+        int lebar;
+        int luas;
+        Scanner masukkan = new Scanner(System.in);
+
+        System.out.print("Masukan panjang : ");
+        panjang = masukkan.nextInt();
+
+        System.out.print("Masukan Lebar   : ");
+        lebar = masukkan.nextInt();
+
+        luas = panjang * lebar;
+
+        System.out.println("Luas persegi panjang adalah: " + luas);
+        System.out.println("");
+    } 
     
     public static void main(String[] args) {
         // TODO code application logic here
